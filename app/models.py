37da55e6b397219c57bd73fe22d28e47749fa2af
Tinayo001 <keiyatinayo@gmail.com>
2024-08-27 16:10:16 +0300

    sokofresh app

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from datetime import datetime
+from datetime import datetime, timezone
 from app import db
 
 class Farmer(db.Model):
@@ -39,7 +39,7 @@ class Product(db.Model):
     description = db.Column(db.Text, nullable=True)
     price = db.Column(db.Float, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
-    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    date_added = db.Column(db.DateTime, default=datetime.now(timezone.utc))  # Updated to use timezone-aware UTC
     
     farmer_id = db.Column(db.Integer, db.ForeignKey('farmers.id'), nullable=False)
     
@@ -52,7 +52,7 @@ class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     quantity = db.Column(db.Integer, nullable=False)
     total_price = db.Column(db.Float, nullable=False)
-    date_ordered = db.Column(db.DateTime, default=datetime.utcnow)
+    date_ordered = db.Column(db.DateTime, default=datetime.now(timezone.utc))  # Updated to use timezone-aware UTC
     
     retailer_id = db.Column(db.Integer, db.ForeignKey('retailers.id'), nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
@@ -68,7 +68,7 @@ class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Float, nullable=False)
     payment_method = db.Column(db.String(50), nullable=False)
-    date_paid = db.Column(db.DateTime, default=datetime.utcnow)
+    date_paid = db.Column(db.DateTime, default=datetime.now(timezone.utc))  # Updated to use timezone-aware UTC
     
     order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), nullable=False)
     
