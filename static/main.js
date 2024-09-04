97ad55aa63dcea5b1736c77432e973a4a52fa605
Tinayo001 <keiyatinayo@gmail.com>
2024-09-04 09:38:02 +0300

    frontend

--- static/main.js
+++
@@ -1,9 +0,0 @@
-// main.js
-
-document.addEventListener('DOMContentLoaded', function() {
-    const btn = document.querySelector('button');
-
-    btn.addEventListener('click', function() {
-        alert('Button was clicked!');
-    });
-});
